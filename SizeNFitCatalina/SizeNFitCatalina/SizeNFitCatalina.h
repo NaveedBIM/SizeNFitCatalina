cecc9789c22ff88523feb45ef2e9306a50e917a1
Naveed ur Rehman <naveed.rehman@bigimmersive.com>
2019-12-19 16:36:15 +0500

    new project

---
+++ SizeNFitCatalina/SizeNFitCatalina/SizeNFitCatalina.h
@@ -0,0 +1,19 @@
+//
+//  SizeNFitCatalina.h
+//  SizeNFitCatalina
+//
+//  Created by Naveed ur Rehman on 19/12/2019.
+//  Copyright © 2019 Naveed ur Rehman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SizeNFitCatalina.
+FOUNDATION_EXPORT double SizeNFitCatalinaVersionNumber;
+
+//! Project version string for SizeNFitCatalina.
+FOUNDATION_EXPORT const unsigned char SizeNFitCatalinaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SizeNFitCatalina/PublicHeader.h>
+
+
